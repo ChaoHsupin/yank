5bfd9205235ca9f5f503c519a390a1909711b1b9
赵栩彬 <zhaoxubin@dxy.cn>
2019-11-22 15:24:21 +0800

    优化密钥拷贝

--- src/main/java/site/yan/key/cmds/Show.java
+++ src/main/java/site/yan/key/cmds/Show.java
@@ -1,7 +1,7 @@
 package site.yan.key.cmds;
 
-import site.yan.key.securtiy.Secrey;
 import site.yan.key.Utils;
+import site.yan.key.securtiy.Secrey;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,8 +11,8 @@ import java.util.*;
 
 import static site.yan.key.Cmd.SECRET_FILE;
 import static site.yan.key.Cmd.arg;
-import static site.yan.key.securtiy.Secrey.secretVerify;
 import static site.yan.key.Utils.contains;
+import static site.yan.key.securtiy.Secrey.secretVerify;
 
 /**
  * Create in 2019/11/22 10:16 by Zhao Xubin.
@@ -21,7 +21,7 @@ public class Show implements CmdBase {
     @Override
     public void execute() {
         secretVerify();
-        if(!arg.equals("#"))
+        if (!arg.equals("#"))
             parse(arg);
         Utils.tell("输入该密钥模糊标题:");
         String title = Utils.input();
@@ -41,46 +41,49 @@ public class Show implements CmdBase {
             String temp;
             while (Objects.nonNull(temp = bufferedReader.readLine())) {
                 String[] line = temp.split(" ");
-                map.put(Secrey.secret(line[0],false), Secrey.secret(line[1],false));
+                map.put(Secrey.secret(line[0], false), Secrey.secret(line[1], false));
             }
             Set<String> set = map.keySet();
-            List<String> list=new ArrayList<>(set);
-            List<String> passwordList=new ArrayList<>();
-            int count=0;
-            for(String head:list){
+            List<String> list = new ArrayList<>(set);
+            List<String> passwordList = new ArrayList<>();
+            int count = 0;
+            for (String head : list) {
                 if (contains(head, regex)) {
                     String[] info = map.get(head).split("\\n");
                     passwordList.add(info[2]);
                     System.out.println();
                     System.out.println(++count);
-                    System.out.println("-------------------");
+                    System.out.println("------------------------");
                     System.out.println("   title:" + info[0]);
                     System.out.println("username:" + info[1]);
                     System.out.println("password:" + info[2]);
                     System.out.println("    memo:" + info[3]);
-                    System.out.println("-------------------");
+                    System.out.println("------------------------");
                 }
             }
             System.out.println();
-            if (list.size()==0) System.out.println("未找到相关密钥 :|");
-            else {
+            if (list.size() == 0) System.out.println("未找到相关密钥 :|");
+            else if (list.size() == 1) {
+                Utils.setClipboardString(passwordList.get(0));
+                Utils.tell("密钥已拷贝到粘贴板", true, true);
+            } else {
                 Utils.tell("请输入序号，密钥将被拷贝到粘贴板：");
-                String order=Utils.input();
-                if(!order.matches("\\d+")){
-                    Utils.tell("编号错误",false,true);
-                }else {
-                    int orderNum=Integer.valueOf(order);
-                    if(orderNum>passwordList.size()){
-                        Utils.tell("编号超出范围",false,true);
-                    }else {
-                        Utils.setClipboardString(passwordList.get(orderNum-1));
-                        Utils.tell("拷贝成功",true,true);
+                String order = Utils.input();
+                if (!order.matches("\\d+")) {
+                    Utils.tell("编号错误", false, true);
+                } else {
+                    int orderNum = Integer.valueOf(order);
+                    if (orderNum > passwordList.size()) {
+                        Utils.tell("编号超出范围", false, true);
+                    } else {
+                        Utils.setClipboardString(passwordList.get(orderNum - 1));
+
                     }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             System.exit(0);
         }
     }
